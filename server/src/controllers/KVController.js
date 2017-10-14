d49eec87ddd7305eca74e8fa040a939d6f2ec15e
Travis Kern <kern.travis@gmail.com>
2017-10-14 16:37:19 -0400

    Move utlity methods in KVController to util module

--- server/src/controllers/KVController.js
+++ server/src/controllers/KVController.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 const bodyParser = require("body-parser");
 
+const ControllerUtil = require("../util/ControllerUtil");
 const kvservice = require("../services/KVService");
 
 const jsonParser = bodyParser.json();
 
 router.get("/all",  (request, response) => {
 	kvservice.getAll((err, rows) => {
-		send(response, rows);
+		ControllerUtil.send(response, rows);
 	});
 });
 
@@ -25,7 +26,7 @@ router.get("/value/:key",  (request, response) => {
 			failInternalError(response);
 			return;
 		}
-		send(response, keys);
+		ControllerUtil.send(response, keys);
 	});
 });
 
@@ -35,7 +36,7 @@ router.get("/keys",  (request, response) => {
 			failInternalError(response);
 			return;
 		}
-		send(response, keys);
+		ControllerUtil.send(response, keys);
 	});
 });
 
@@ -54,7 +55,7 @@ router.put("/value", jsonParser, (request, response) => {
 			failInternalError(response);
 			return;
 		}
-		send(response, undefined);
+		ControllerUtil.send(response, undefined);
 	});
 });
 
@@ -72,28 +73,12 @@ router.get("/keysWithValue/:value",  (request, response) => {
 			failInternalError(response);
 			return;
 		}
-		send(response, keys);
+		ControllerUtil.send(response, keys);
 	});
 });
 
 router.all("*", (request, response) => {
 	failUnknownRoute(response);
 });
-
-function send(response, data) {
-	response.json(data);
-}
-
-function failParam(response) {
-	response.status(400).json({message: "Missing parameter"});
-}
-
-function failUnknownRoute(response) {
-	response.status(404).json({message: "Unknown route"});
-}
-
-function failInternalError(response) {
-	response.status(500).json({message: "An internal error occurred."});
-}
 
 module.exports = exports = router;
---
+++ server/src/util/ControllerUtil.js
@@ -0,0 +1,17 @@
+module.exports = exports = {
+	send: (response, data) => {
+		response.json(data);
+	},
+	
+	failParam: (response) => {
+		response.status(400).json({message: "Missing parameter"});
+	},
+	
+	failUnknownRoute: (response) => {
+		response.status(404).json({message: "Unknown route"});
+	},
+	
+	failInternalError: (response) => {
+		response.status(500).json({message: "An internal error occurred."});
+	}
+};
