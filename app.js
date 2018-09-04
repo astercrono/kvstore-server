aef34a76ee18e60b328d9737dba81e27f81f34d1
Travis Kern <kern.travis@gmail.com>
2018-09-03 21:29:32 -0400

    Print API key if running in test mode

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ if (process.argv.length === 3) {
 	profile = process.argv[2];
 }
 
-console.log("starting kvstore-server");
-
+const Config = require("./src/config/Config");
+const KeyStore = require("./src/crypt/KeyStore");
 const Initializer = require("./src/Initializer");
+
+console.log("starting kvstore-server");
 Initializer.run(profile, () => {
 	console.log("kvstore-server is up and running");
+
+	if (Config.name === "test") {
+		console.log("API Key = " + KeyStore.get("api"));
+	}
 });
